33bd95ecefa1cb3041b5c115d72cebc403d0e434
Bruno Soares <85403673+felipebss1@users.noreply.github.com>
2023-03-30 17:24:20 -0300

    Altered Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/pages/auth_page.dart';
 import 'package:provider/provider.dart';
 
 import 'package:flutter_shop_app/pages/cart_page.dart';
@@ -42,7 +43,7 @@ class MyApp extends StatelessWidget {
           ),
           fontFamily: 'Lato',
         ),
-        home: const ProductsOverviewPage(),
+        home: const AuthPage(),
         routes: {
           ProductDetailPage.routeName: (context) => const ProductDetailPage(),
           CartPage.routeName: (context) => const CartPage(),
@@ -54,3 +55,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+//Parou na 266 -> Não esquecer de ler!
