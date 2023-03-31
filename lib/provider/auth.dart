46f9a38aa44f7cf0ceaad29f46d089262addc3fc
Bruno Soares <85403673+felipebss1@users.noreply.github.com>
2023-03-31 17:00:10 -0300

    Brand New Component

---
+++ lib/provider/auth.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  late String _token;
+  late DateTime _expiryDate;
+  late String _userId;
+
+  Future<void> signUp(String email, String password) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBMqnCr3SoD7e7etgSdYTs_FRa-2DpvTW8');
+    final response = await http.post(
+      url,
+      body: jsonEncode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(jsonDecode(response.body));
+  }
+}
