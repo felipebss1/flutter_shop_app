67ada72dd5b8c6498df0dba6d8fcaaebb3b9455e
Bruno Soares <85403673+felipebss1@users.noreply.github.com>
2023-04-03 17:35:39 -0300

    Added ProxyProvider

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter_shop_app/pages/cart_page.dart';
 import 'package:flutter_shop_app/pages/edit_product_page.dart';
 import 'package:flutter_shop_app/pages/orders_page.dart';
 import 'package:flutter_shop_app/pages/product_detail_page.dart';
-//import 'package:flutter_shop_app/pages/products_overview_page.dart';
+import 'package:flutter_shop_app/pages/products_overview_page.dart';
 import 'package:flutter_shop_app/pages/user_products_page.dart';
 import 'package:flutter_shop_app/provider/auth.dart';
 import 'package:flutter_shop_app/provider/cart.dart';
@@ -27,34 +27,45 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (context) => Auth(),
         ),
-        ChangeNotifierProvider(
-          create: (context) => Products(),
+        ChangeNotifierProxyProvider<Auth, Products>(
+          create: (_) => Products('', '', []),
+          update: (context, auth, previousProducts) => Products(
+            auth.token!,
+            auth.userId,
+            previousProducts == null ? [] : previousProducts.items,
+          ),
         ),
         ChangeNotifierProvider(
           create: (context) => Cart(),
         ),
-        ChangeNotifierProvider(
-          create: (context) => Orders(),
+        ChangeNotifierProxyProvider<Auth, Orders>(
+          create: (_) => Orders('', []),
+          update: (context, auth, previousOrders) => Orders(
+            auth.token!,
+            previousOrders == null ? [] : previousOrders.orders,
+          ),
         ),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'My Shop',
-        theme: ThemeData(
-          colorScheme:
-              ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
-            secondary: Colors.deepOrange,
+      child: Consumer<Auth>(
+        builder: (context, auth, _) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'My Shop',
+          theme: ThemeData(
+            colorScheme:
+                ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
+              secondary: Colors.deepOrange,
+            ),
+            fontFamily: 'Lato',
           ),
-          fontFamily: 'Lato',
+          home: auth.isAuth ? const ProductsOverviewPage() : const AuthPage(),
+          routes: {
+            ProductDetailPage.routeName: (context) => const ProductDetailPage(),
+            CartPage.routeName: (context) => const CartPage(),
+            OrdersPage.routeName: (context) => const OrdersPage(),
+            UserProductsPage.routeName: (context) => const UserProductsPage(),
+            EditProductPage.routeName: (context) => const EditProductPage(),
+          },
         ),
-        home: const AuthPage(),
-        routes: {
-          ProductDetailPage.routeName: (context) => const ProductDetailPage(),
-          CartPage.routeName: (context) => const CartPage(),
-          OrdersPage.routeName: (context) => const OrdersPage(),
-          UserProductsPage.routeName: (context) => const UserProductsPage(),
-          EditProductPage.routeName: (context) => const EditProductPage(),
-        },
       ),
     );
   }
